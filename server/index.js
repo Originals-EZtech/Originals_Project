ab9941c941f34a112a4ce36531852ba509f84abc
sukhyunil <sukhyunil@naver.com>
2021-12-14 14:56:25 +0900

    modified thcode related mybatis

--- server/index.js
+++ server/index.js
@@ -7,15 +7,16 @@ app.use(express.json());
 
 // DB 설정
 const oracledb = require('oracledb');
-const dbConfig = require('./dbConfig');
-
+const dbConfig = require('./config/dbConfig');
+const mybatisConfig = require('./config/mybatisConfig')
 oracledb.autoCommit = true;
 
 // mybatis-mapper 추가
 const mybatisMapper = require('mybatis-mapper');
 
 // Mapper Load
-// mybatisMapper.createMapper(['C:\Users\user\Originals-EZtech\Originals_Project\server\mybatis\oracle-mapper.xml']);
+mybatisMapper.createMapper([mybatisConfig.mybatisURL]);
+console.log([mybatisConfig.mybatisURL]);
 
 
 
@@ -26,18 +27,16 @@ app.get('/', (req, res) => res.send('Hello World ~~~~~ HIHI'));
 app.get('/api/hello', (req, res) => {
     oracledb.getConnection(dbConfig, (err, conn) => {
         todoWork(err, conn);
-        console.log('visited~~~~~~~~~~~~~~~~~~~~~~~~')
     });
     
     function todoWork(err, connection) {
-        console.log(1,"todoWorkdotdotdot"); 
         if (err) {
             console.error(err.message);
             return;
         }
     
         let format = {language: 'sql', indent: ' '};
-        // let query = mybatisMapper.getStatement('oracleMapper', 'selectUserInfo', null, format)
+        let query = mybatisMapper.getStatement('oracleMapper', 'selectUserInfo', null, format)
     
         connection.execute(query, function (err, result) {
             if (err) {
