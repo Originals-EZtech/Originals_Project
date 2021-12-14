a412c97baa94e0e2d05f26944410052c149ae1b5
sukhyunil <sukhyunil@naver.com>
2021-12-14 14:11:28 +0900

    updated mainpage, indes.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ oracledb.autoCommit = true;
 const mybatisMapper = require('mybatis-mapper');
 
 // Mapper Load
-mybatisMapper.createMapper(['C:/Users/user/Desktop/Project/Originals_Project__/Originals-Project/server/mybatis/oracle-mapper.xml']);
+// mybatisMapper.createMapper(['C:\Users\user\Originals-EZtech\Originals_Project\server\mybatis\oracle-mapper.xml']);
 
 
 
@@ -26,16 +26,18 @@ app.get('/', (req, res) => res.send('Hello World ~~~~~ HIHI'));
 app.get('/api/hello', (req, res) => {
     oracledb.getConnection(dbConfig, (err, conn) => {
         todoWork(err, conn);
+        console.log('visited~~~~~~~~~~~~~~~~~~~~~~~~')
     });
     
     function todoWork(err, connection) {
+        console.log(1,"todoWorkdotdotdot"); 
         if (err) {
             console.error(err.message);
             return;
         }
     
         let format = {language: 'sql', indent: ' '};
-        let query = mybatisMapper.getStatement('oracleMapper', 'selectUserInfo', null, format)
+        // let query = mybatisMapper.getStatement('oracleMapper', 'selectUserInfo', null, format)
     
         connection.execute(query, function (err, result) {
             if (err) {
@@ -44,6 +46,7 @@ app.get('/api/hello', (req, res) => {
                 return;
             }
             console.log(result.rows);  //데이터
+            console.log('DB 연결 성공');  
 
             res.send(result.rows);
             doRelease(connection, result.rows);
