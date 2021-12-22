55a5fa1e4f3ba73592aa532febaf1fa48fd68ce4
ynsseon07 <ynsseon@gmail.com>
2021-12-22 17:56:16 +0900

    update_1222_ys

--- client/src/components/views/Room/Room.js
+++ client/src/components/views/Room/Room.js
@@ -1,15 +1,24 @@
 import React from 'react';
 // import { Route, Link } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import RoomNavBar from '../NavBar/RoomNavBar';
+import styles from '../Room/room.module.css';
 
 
 function Room() {
 
     return (
         <div>
-            <h3><li><Link to="/roomcreate" >방 만들기</Link></li></h3>
-            
-            <h3><li><Link to="/roomjoin" >방 참여하기</Link></li></h3>
+            <RoomNavBar />
+            <div style={{marginTop: 150}}></div>
+            <div class="wrap">
+                <div class="createRoom">
+                    <h3><li><Link to="/roomcreate" >방 만들기</Link></li></h3>
+                </div>
+                <div class="enterRoom">
+                    <h3><li><Link to="/roomjoin" >방 참여하기</Link></li></h3>
+                </div>
+            </div>
         </div>
     );
 };
