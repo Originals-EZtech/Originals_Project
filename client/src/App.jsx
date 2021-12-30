ac8a78f6e10fd061fca6cb3b6b1e25ad67a90b09
ynsseon07 <ynsseon@gmail.com>
2021-12-30 19:28:18 +0900

    update_1230

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,12 @@ import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import JoinRoomPage from './components/views/Room/JoinRoomPage/JoinRoomPage';
 import RoomPage from './components/views/Room/RoomPage/RoomPage';
 import IntroductionPage from './components/views/Room/IntroductionPage/IntroductionPage';
-// import Room from './components/views/Room/Room';
+// import Room from './components/views/Room_By_Dong/Room';
 // import RoomCreate from './components/views/Room/RoomCreate/RoomCreate';
 // import RoomJoin from './components/views/Room/RoomJoin/RoomJoin';
 // import RoomAdmin from './components/views/Room/RoomCreate/RoomAdmin';
 // import RoomParticipant from './components/views/Room/RoomJoin/RoomParticipant';
-import Auth from './hoc/auth'
+import Auth from './hoc/auth';
 
 import { connectWithSocketIOServer} from './components/views/Room/utils/wss';
 
@@ -45,16 +45,16 @@ const AnimatedSwitch = () => {
     <TransitionGroup component={null}>
       <CSSTransition key={location.key} classNames="fade" timeout={500}>
         <Switch location={location}>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/" component={Auth(MainPage, null)} />
+        <Route exact path="/login" component={Auth(LoginPage, false)} />
         <Route exact path="/register" component={RegisterPage} />
 
         <Route exact path = "/join-room" component = {JoinRoomPage} />
         <Route exact path ="/room" component ={RoomPage} />
         <Route exact path ="/intro" component = {IntroductionPage} />
 
-        {/* <Route exact path="/room" component={Auth(Room, null )  } />
-        <Route exact path="/roomcreate" component={Auth(RoomCreate, null )  } />
+        {/* <Route exact path="/room_test" component={Room} /> */}
+        {/* <Route exact path="/roomcreate" component={Auth(RoomCreate, null )  } />
         <Route exact path="/RoomJoin" component={Auth(RoomJoin, null )  } />
         <Route exact path="/RoomAdmin" component={Auth(RoomAdmin, null )  } />
         <Route exact path="/RoomParticipant" component={Auth(RoomParticipant, null )  } /> */}
