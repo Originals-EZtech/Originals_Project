380b71736052e0b4f8a6ea0bc13fd2003235d1dd
dongsushin <sjagz@naver.com>
2021-12-30 17:51:29 +0900

    12_webRTCinfra

--- client/src/components/views/Room/JoinRoomPage/JoinRoomButtons.js
+++ client/src/components/views/Room/JoinRoomPage/JoinRoomButtons.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Button = ({ buttonText, cancelButton = false, onClickHandler }) =>{
     const buttonClass = cancelButton? 'join_room_cancel_button' : 'join_room_success_button';
@@ -11,9 +12,18 @@ const Button = ({ buttonText, cancelButton = false, onClickHandler }) =>{
         </button>
 );
 };
-const JoinRoomButtons = (props, {handleJoinRoom, isRoomHost}) => {
-    const successButtonText = isRoomHost ? 'Host' : 'Join';
 
+//props 부분 지식과 true, false 이외의 값이 전달됐을때 오류메세지 던져주기
+const JoinRoomButtons = (props) => {
+    // console.log("isRoomHost :: "+isRoomHost)
+    // isRoomHost=true;
+    // console.log("isRoomHost :: "+isRoomHost)
+    // console.log("props ::"+ props)
+    // console.log("handleJoinRoom ::"+ handleJoinRoom)
+    // console.log("##############")
+    const successButtonText = props.isRoomHost ? 'Host' : 'Join';
+    
+    
     const pushToIntroductionPage = () => {
         props.history.push('/intro');
     };
@@ -22,8 +32,9 @@ const JoinRoomButtons = (props, {handleJoinRoom, isRoomHost}) => {
         <div className='join_room_buttons_container'>
             <Button
             buttonText = {successButtonText}
-            onClickHandler={handleJoinRoom} 
+            onClickHandler={props.handleJoinRoom} 
             />
+            <Link to ="/intro" >버튼동작</Link>
             <Button
             buttonText = 'Cancel'
             cancelButton
--- client/src/index_origin.js
+++ client/src/index_origin.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-import Reducer from './_reducers';
-import { CookiesProvider } from 'react-cookie';
+// import { Provider } from 'react-redux';
+// import { applyMiddleware, createStore } from 'redux';
+// import promiseMiddleware from 'redux-promise';
+// import ReduxThunk from 'redux-thunk';
+// import Reducer from './_reducers';
+// import { CookiesProvider } from 'react-cookie';
 
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+// const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(Reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
-  )}>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>,
-  </Provider>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(Reducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//     window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )}>
+//     <CookiesProvider>
+//       <App />
+//     </CookiesProvider>,
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
