32626af3d6ff24bba1e43fe8b5c89a578c8bb9c1
nagggyung <skql771@gmail.com>
2022-01-03 12:57:51 +0900

    add dotenv

--- client/src/components/views/Room/JoinRoomPage/JoinRoomInputs.js
+++ client/src/components/views/Room/JoinRoomPage/JoinRoomInputs.js
@@ -26,13 +26,13 @@ const JoinRoomInputs = (props) =>{
     }
     return <div className = 'join_room_inputs_container'>
         {!isRoomHost && (<Input 
-        placeholder= '방 비밀번호'
+        placeholder= 'Enter meeting ID'
         value = {roomIdValue}
         changeHandler={handleRoomIdValueChange}
         />
         )}
         <Input 
-         placeholder = '방 ID'
+         placeholder = 'Enter your Name'
          value = {nameValue}
          changeHandler={handleNameValueChange}
         />
