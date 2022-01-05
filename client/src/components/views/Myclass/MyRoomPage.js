ef6e31a3150916059cf81aa1d28a9f924212d0d6
ynsseon07 <ynsseon@gmail.com>
2022-01-05 18:37:08 +0900

    update_0105

--- client/src/components/views/Myclass/MyRoomPage.js
+++ client/src/components/views/Myclass/MyRoomPage.js
@@ -1,10 +1,13 @@
 import React, { useState, } from 'react';
+import RoomNavBar from '../NavBar/RoomNavBar';
 import RoomListComponent from './RoomListComponent';
 
 function JoinRoomComponent() {
 
     return (
         <div>
+            <RoomNavBar />
+            <div style={{paddingTop: 100}}></div>
             <h3>만들었던 방 목록</h3><br></br>   
             <h3><RoomListComponent /></h3>
 
