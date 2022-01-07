a158939f190ec7a2bc8c2d84d26428033e128538
ynsseon07 <ynsseon@gmail.com>
2022-01-07 21:25:11 +0900

    update_test

--- client/src/config/ipconfig.js
+++ client/src/config/ipconfig.js
@@ -4,6 +4,7 @@ module.exports =
     //server:'http://172.30.1.44:5000'
 
     //5G
-    server:'http://172.30.1.55:5000'
+    // server:'http://172.30.1.55:5000'
 
+    server:'http://192.168.68.81:5000'
 }
