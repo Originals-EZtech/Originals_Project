66d00b5b28aa0872581cc8d0778796cee4ee1426
sukhyunil <sukhyunil@naver.com>
2022-01-07 20:03:08 +0900

    deleted unsuesd js

--- server/test.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path : 
-    "./../.env" 
-});
-
-const apiKey = process.env.TWILIO_ACCOUNT_SID;
-const secret = process.env.TWILIO_AUTH_TOKEN;
-
-console.log(apiKey);
-console.log(secret);
