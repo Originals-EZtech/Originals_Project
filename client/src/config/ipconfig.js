9c677929ffd5d535a6915c4ec81e39a7da2117ba
nagggyung <skql771@gmail.com>
2022-01-08 23:54:47 +0900

    commit

--- client/src/config/ipconfig.js
+++ client/src/config/ipconfig.js
@@ -6,9 +6,5 @@ module.exports =
     //5G
     // server:'http://172.30.1.55:5000'
 
-<<<<<<< HEAD
     server:'http://192.168.0.105:5000'
-=======
-    server:'http://192.168.219.102:5000'
->>>>>>> ac4b342f0fb1877ed7999606e853e2c979382eae
 }
